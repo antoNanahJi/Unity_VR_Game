ea7f21592788e961980b1c09134c78191a24e721
antoNanahJi <Anto.NanahJi@georgebrown.ca>
2018-02-13 12:19:54 -0500

    Hand Follows Mouse

--- Game2015_VR/Assets/Scripts/Player.cs
+++ Game2015_VR/Assets/Scripts/Player.cs
@@ -7,8 +7,10 @@ public class Player : MonoBehaviour {
 	[SerializeField] float MovementSpeed=5.0f;
 	[SerializeField] float RotationSpeed=45.0f;
 	[SerializeField] GameObject myHand;
+	public float DistanceHand=0.4f;
 	private bool canGrapObject = true;
 	private bool isTarget=false;
+	private Vector3 mousePosition;
 	private GameObject Target = null;
 	Ray mRay;
 	RaycastHit mHit;
@@ -17,9 +19,21 @@ public class Player : MonoBehaviour {
 	void Start () {
 		mHit = new RaycastHit ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		HandFollowMouse ();
+		MovePlayer ();
+		GrabDropObject ();
+	}
+	void HandFollowMouse()
+	{
+		mousePosition = Input.mousePosition;
+		mousePosition.z = DistanceHand;
+		myHand.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+	}
+	void MovePlayer()
+	{
 		if (Input.GetKey (KeyCode.W)) {
 			transform.Translate (Vector3.forward*MovementSpeed*Time.deltaTime);
 		}
@@ -32,6 +46,9 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey (KeyCode.D)) {
 			transform.Rotate (Vector3.up*RotationSpeed*Time.deltaTime);
 		}
+	}
+	void GrabDropObject()
+	{
 		mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		if (Input.GetMouseButton (0)) {
 			if (canGrapObject) {
@@ -51,16 +68,14 @@ public class Player : MonoBehaviour {
 		}
 		if (Input.GetMouseButton (1)) {
 			if (!canGrapObject) {
-				if (Physics.Raycast (mRay, out mHit, Mathf.Infinity)) {
+				if (Physics.Raycast (mRay, out mHit,Distance)) {
 					Target.transform.position = new Vector3 (mHit.transform.position.x,
 						mHit.transform.position.y,
 						mHit.transform.position.z);
-						isTarget = false;
-						canGrapObject = true;
-					}
+					isTarget = false;
+					canGrapObject = true;
 				}
 			}
-
-
+		}
 	}
 }
