a6fd75782c8bd0f423ad5ad50b8fbc11aceca202
antoNanahJi <Anto.NanahJi@georgebrown.ca>
2018-03-19 23:22:34 -0400

    Adding Oven/Jump Scare

---
+++ Game2015_VR/Assets/Scripts/AntoScript/Ghost.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour {
+	[SerializeField] GameObject target;
+	[SerializeField] float Speed;
+	Quaternion destRot;
+	float speedFactor;
+	float disTo;
+	Vector3 destVector;
+
+	// Update is called once per frame
+	void Update () {
+		destVector = target.transform.position - transform.position;
+		destRot = Quaternion.LookRotation (destVector);
+		disTo = destVector.magnitude;
+		transform.rotation = Quaternion.RotateTowards (transform.rotation,destRot,45.0f*Time.deltaTime);
+		speedFactor=(Mathf.Clamp((disTo/5.0f),0.01f,1.0f));
+		transform.Translate (Vector3.forward*(speedFactor*Speed*Time.deltaTime));
+		Destroy (this.gameObject, 0.5f);
+	}
+}
---
+++ Game2015_VR/Assets/Scripts/AntoScript/Oven_Script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oven_Script: MonoBehaviour {
+	Animator anim;
+	AudioSource aud;
+	[SerializeField] Light light;
+	[SerializeField] Transform ghostSP;
+	[SerializeField] GameObject ghost;
+
+	// Use this for initialization
+	void Start () {
+		anim = this.gameObject.GetComponent<Animator> ();
+		aud = this.gameObject.GetComponent<AudioSource> ();
+		anim.enabled = false;
+		light.enabled = false;
+		aud.Pause ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag=="Player")
+		{
+			anim.enabled = true;
+			aud.Play ();
+			light.enabled = true;
+			Instantiate (ghost,ghostSP.transform.position,ghostSP.transform.rotation);
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag=="Player")
+		{
+			aud.Pause ();
+			anim.enabled = false;
+			light.enabled = false;
+		}
+	}
+}
