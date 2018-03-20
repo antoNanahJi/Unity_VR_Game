f683264c0d560ad3f4c71dbcfb0e774d470fde8f
AndreR92 <andre.reano@hotmail.com>
2018-03-19 21:48:18 -0400

    puzzle addons/script fix

---
+++ Game2015_VR/Assets/Scripts/riddlePuzzle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riddlePuzzle : MonoBehaviour {
+	[SerializeField] GameObject sunObject;
+	[SerializeField] GameObject moonObject;
+	[SerializeField] GameObject starObject;
+
+	private int counter = 0;
+	private int counterMax = 3;
+
+	void Start()
+	{
+		sunObject = GameObject.FindWithTag("sunToy");
+		moonObject = GameObject.FindWithTag("moonToy");
+		starObject = GameObject.FindWithTag("starToy");
+	}
+
+	// Triggers when proper object is placed on pedestal
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.CompareTag ("sunSpot")) 
+		{
+			if (counter < counterMax) {
+				counter++;
+				// Display correct object messeage
+			}
+
+			if (counter == counterMax) {
+				// All objects placed, puzzle solved
+			}
+		}
+
+		if (col.CompareTag ("moonSpot")) {
+			if (counter < counterMax) {
+				counter++;
+				// Display correct object messeage
+			}
+
+			if (counter == counterMax) {
+				// All objects placed, puzzle solved
+			}
+		}
+
+		if (col.CompareTag ("starSpot")) {
+			if (counter < counterMax) {
+				counter++;
+				// Display correct object messeage
+			}
+
+			if (counter == counterMax) {
+				// All objects placed, puzzle solved
+			}
+		}
+	}
+}
