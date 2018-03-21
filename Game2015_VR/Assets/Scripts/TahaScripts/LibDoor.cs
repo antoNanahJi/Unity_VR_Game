3d9d6c05cc6dc046a9c9fbdee7dc0b5b2713256f
kalander1 <tsaleem2@georgebrown.ca>
2018-03-21 11:11:25 -0400

    minor fixes to the library puzzle

--- Game2015_VR/Assets/Scripts/TahaScripts/LibDoor.cs
+++ Game2015_VR/Assets/Scripts/TahaScripts/LibDoor.cs
@@ -17,7 +17,7 @@ public class LibDoor: MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.E)&& doorClosed) {
+		if (Input.GetKeyDown(KeyCode.Mouse1)&& doorClosed) {
 			door.transform.Rotate(0.0f,-90.0f,0.0f);
 
 			Invoke ("doorAuto", 4.0f);
