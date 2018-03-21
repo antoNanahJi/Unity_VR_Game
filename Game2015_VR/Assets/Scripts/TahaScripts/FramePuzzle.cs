539ccf8a752f3057572bb3899447635f32f81597
kalander1 <tsaleem2@georgebrown.ca>
2018-03-21 08:38:39 -0400

    Library puzzle + anticepation
    Finished library puzzle, and anticepation, also added sounds for intro and end

---
+++ Game2015_VR/Assets/Scripts/TahaScripts/FramePuzzle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FramePuzzle : MonoBehaviour {
+
+	static bool fOne = false;
+	static bool fTwo = false;
+	static bool fThree = false;
+	static bool fFour = false;
+	public GameObject libDoor;
+	//public GameObject snapsy;
+	static bool stop = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fOne &&fTwo&&fThree&&fFour && stop) {
+			stop = false;
+			libDoor.transform.Rotate (0.0f, -90.0f, 0.0f);
+
+		}
+		Debug.Log (fOne + " 1");
+		Debug.Log (fTwo + " 2");
+		Debug.Log (fThree + " 3");
+		Debug.Log (fFour + " 4");
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		other.gameObject.transform.position = this.transform.position;
+
+		if (other.gameObject.name == "eLet" && this.gameObject.name == "Frame1") {
+			fOne = true;
+		}
+		if (other.gameObject.name == "zLet" && this.gameObject.name == "Frame2")
+			fTwo = true;
+		if (other.gameObject.name == "oLet" && this.gameObject.name == "Frame3")
+			fThree = true;
+		if (other.gameObject.name == "nLet" && this.gameObject.name == "Frame4")
+			fFour = true;
+	}
+
+}
---
+++ Game2015_VR/Assets/Scripts/TahaScripts/LibDoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibDoor: MonoBehaviour {
+
+	bool doorClosed = false;
+	public GameObject door;
+	AudioSource doorSound;
+	public AudioSource knocking;
+	public GameObject audioHolder;
+	// Use this for initialization
+	void Start () {
+		doorSound = this.gameObject.GetComponent<AudioSource> ();
+		knocking = audioHolder.gameObject.GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.E)&& doorClosed) {
+			door.transform.Rotate(0.0f,-90.0f,0.0f);
+
+			Invoke ("doorAuto", 4.0f);
+
+		}
+
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+		doorClosed = true;
+	
+	}
+	void OnTriggerExit(Collider other)
+	{
+		doorClosed = false;
+	}
+	void doorAuto()
+	{
+		door.transform.Rotate(0.0f,90.0f,0.0f);
+		doorSound.Play ();
+		Invoke ("knockingAudio", 10.0f);
+	}
+	void knockingAudio()
+	{
+		knocking.Play ();
+	}
+
+
+}
