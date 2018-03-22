274eb7bb7024102bfc83ca9b369dc9b74b0c90fb
8tone <logankingltd@gmail.com>
2018-03-22 12:09:53 -0400

    minor layout/ puzzle implementation

---
+++ Game2015_VR/Assets/Scripts/LoganScripts/bookPuzzle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bookPuzzle : MonoBehaviour {
+	
+
+
+
+	void Start()
+	{
+		
+
+	}
+	void OnTriggerEnter(Collider _collision){
+		if (_collision.gameObject.tag == "bookSpot") 
+		{
+			Debug.Log ("placed book");
+		}
+	
+
+
+
+	}
+}
