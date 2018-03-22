2203fbc66c9a33b2fa073895bddc2ce17d38ad93
AndreR92 <andre.reano@hotmail.com>
2018-03-21 22:52:50 -0400

    Andre puzzle fix

---
+++ Game2015_VR/Assets/Scripts/AndreScript/riddlePuzzle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class riddlePuzzle : MonoBehaviour {
+	[SerializeField] GameObject sunObject;
+	[SerializeField] GameObject moonObject;
+	[SerializeField] GameObject starObject;
+	[SerializeField] GameObject sunSpot;
+	[SerializeField] GameObject moonSpot;
+	[SerializeField] GameObject starSpot;
+
+	private bool sunCheck = false;
+	private bool moonCheck = false;
+	private bool starCheck = false;
+
+	void Start()
+	{
+		sunObject = GameObject.Find ("Sun");
+		moonObject = GameObject.Find ("Moon");
+		starObject = GameObject.Find ("Star");
+		sunSpot = GameObject.Find ("PedestalSun");
+		moonSpot = GameObject.Find ("PedestalMoon");
+		starSpot = GameObject.Find ("PedestalStar");
+	}
+
+	// Triggers when proper object is placed on pedestal
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (sunSpot == sunObject) {
+			sunCheck = true;
+			Debug.Log (sunCheck);
+		}
+		
+	}
+}
