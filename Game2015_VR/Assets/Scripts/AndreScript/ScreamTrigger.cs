1eea97ab28e4fb9ecb141900427e82b943a38880
AndreR92 <andre.reano@hotmail.com>
2018-03-23 02:11:47 -0400

    Jump Scare Complete

--- Game2015_VR/Assets/Scripts/AndreScript/ScreamTrigger.cs
+++ Game2015_VR/Assets/Scripts/AndreScript/ScreamTrigger.cs
@@ -2,31 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/*public class ScreamTrigger : riddlePuzzle {
+public class ScreamTrigger : riddlePuzzle {
 
-	private GameObject triggerObject;
-	private AudioSource _Scream;
+	public Collider screamTrigger;
+	public GameObject slenderMan;
+	public AudioSource _Scream;
+
 
 	// Use this for initialization
 	void Start () {
-		//triggerObject = GetComponent<BoxCollider> ();
+		slenderMan = GameObject.Find ("SlenderMan");
+		slenderMan.SetActive (false);
+
+		screamTrigger = this.gameObject.GetComponent<Collider>();
+		screamTrigger.enabled = false;
+
 		_Scream = this.gameObject.GetComponent <AudioSource> ();
-		//triggerObject.SetActive (false);
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (sunCheck == true && moonCheck == true && starCheck == true) {
-			//triggerObject.SetActive (true);
+			screamTrigger.enabled = true;
 		}
 	}
 
 	void OnTriggerEnter(Collider collide)
 	{
-		if (collide.CompareTag("Player")) {
-			_Scream.Play ();
+		if (collide.CompareTag("Player")) 
+		{
+			SlenderPlay ();
 		}
 	}
 
+	void SlenderPlay()
+	{
+		//triggerObject.enabled = false;
+		_Scream.Play ();
+		slenderMan.SetActive (true);
+		Destroy (this.gameObject, 3.0f);
+	}
 }
-*/
