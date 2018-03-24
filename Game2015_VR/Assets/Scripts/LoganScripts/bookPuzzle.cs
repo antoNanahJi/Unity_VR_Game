510e19a06a7e462a720b697708d1ddfb0002bf4a
8tone <logankingltd@gmail.com>
2018-03-23 20:49:48 -0400

    puzzle implementation/classroomSounds

--- Game2015_VR/Assets/Scripts/LoganScripts/bookPuzzle.cs
+++ Game2015_VR/Assets/Scripts/LoganScripts/bookPuzzle.cs
@@ -3,23 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class bookPuzzle : MonoBehaviour {
-	
+	[SerializeField]GameObject scroll1;
+	[SerializeField]GameObject scroll2;
+	[SerializeField]GameObject scroll3;
+	[SerializeField]GameObject scroll4;
+
+
+	private AudioSource complete;
 
 
 
 	void Start()
 	{
-		
+		complete = gameObject.GetComponent<AudioSource> ();
 
 	}
 	void OnTriggerEnter(Collider _collision){
-		if (_collision.gameObject.tag == "bookSpot") 
+		if (_collision.gameObject.name == "bookSpot1") 
 		{
-			Debug.Log ("placed book");
+			Debug.Log ("placed book1");
+			complete.Play ();
+			Instantiate (scroll1,scroll1.transform.position ,scroll1.transform.rotation);
 		}
-	
-
-
-
+		if (_collision.gameObject.name == "bookSpot2") 
+		{
+			Debug.Log ("placed book2");
+			complete.Play ();
+			Instantiate (scroll2,scroll2.transform.position ,scroll2.transform.rotation);
+		}
+		if (_collision.gameObject.name == "bookSpot3") 
+		{
+			Debug.Log ("placed book3");
+			complete.Play ();
+			Instantiate (scroll3,scroll3.transform.position ,scroll3.transform.rotation);
+		}
+		if (_collision.gameObject.name == "bookSpot3") 
+		{
+			Debug.Log ("placed book3");
+			complete.Play ();
+			Instantiate (scroll3,scroll3.transform.position ,scroll3.transform.rotation);
+		}
 	}
+
+
 }
---
+++ Game2015_VR/Assets/Scripts/LoganScripts/placementScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placementScript : MonoBehaviour {
+	[SerializeField] GameObject scrollPlaced1;
+	[SerializeField] GameObject scrollPlaced2;
+	[SerializeField] GameObject scrollPlaced3;
+	[SerializeField] GameObject scrollPlaced4;
+	private AudioSource complete;
+	int count = 0;
+	// Use this for initialization
+	void Start () {
+		complete = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider _collision){
+		if (_collision.gameObject.name == "scroll1") 
+		{
+			Instantiate (scrollPlaced1,scrollPlaced1.transform.position ,scrollPlaced1.transform.rotation);
+			complete.Play ();
+			count++;
+		}
+		if (_collision.gameObject.name == "scroll2") 
+		{
+			Instantiate (scrollPlaced2,scrollPlaced2.transform.position ,scrollPlaced2.transform.rotation);
+			complete.Play ();
+			count++;
+		}
+		if (_collision.gameObject.name == "scroll3") 
+		{
+			
+
+			Instantiate (scrollPlaced3,scrollPlaced3.transform.position ,scrollPlaced3.transform.rotation);
+			complete.Play ();
+			count++;
+		}
+		if (_collision.gameObject.name == "scroll4") 
+		{
+			
+
+			Instantiate (scrollPlaced4,scrollPlaced3.transform.position ,scrollPlaced3.transform.rotation);
+			complete.Play ();
+			count++;
+		}
+		if (count == 4) 
+		{
+			Debug.Log ("Completed");
+		}
+	}
+}
