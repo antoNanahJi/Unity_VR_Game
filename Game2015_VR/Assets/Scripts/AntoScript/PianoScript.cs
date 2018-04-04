4731214e6bd31dba2fc85f7d39c29a6cdd77ca5c
antoNanahJi <Anto.NanahJi@georgebrown.ca>
2018-04-04 13:42:24 -0400

    New Ghost & Fixing Rails

---
+++ Game2015_VR/Assets/Scripts/AntoScript/PianoScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoScript : MonoBehaviour {
+	Animator anim;
+	AudioSource aud;
+	[SerializeField] AudioClip [] clips;
+	int random;
+
+	// Use this for initialization
+	void Start () {
+		aud = this.gameObject.GetComponent<AudioSource> ();
+		anim = this.gameObject.GetComponent<Animator> ();
+		anim.enabled = false;
+	}
+
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag=="Player")
+		{
+			anim.enabled = true;
+			random = Random.Range (0,3);
+			aud.clip=clips[random];
+			aud.Play ();
+
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag=="Player")
+		{
+			
+			aud.Pause ();
+			anim.enabled = false;
+		}
+	}
+
+		
+}
