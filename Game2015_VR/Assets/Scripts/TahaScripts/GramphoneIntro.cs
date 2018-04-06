bff8d907211dbcde719a221466c01fb201c9379c
kalander1 <tsaleem2@georgebrown.ca>
2018-04-06 01:41:36 -0400

    Fixed the intro sound

---
+++ Game2015_VR/Assets/Scripts/TahaScripts/GramphoneIntro.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GramphoneIntro : MonoBehaviour {
+	AudioSource BackgroundMusic;
+	GameObject player;
+	AudioSource playerMusic;
+	bool music = false;
+	// Use this for initialization
+	void Start () {
+
+			player = GameObject.FindWithTag ("Player");
+			playerMusic = player.GetComponent<AudioSource> ();
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		if (OVRInput.Get (OVRInput.Axis1D.SecondaryHandTrigger) >= 1 && music) 
+		{
+			playerMusic.Stop ();
+			BackgroundMusic.Stop ();
+			BackgroundMusic = this.gameObject.GetComponent<AudioSource> ();
+			BackgroundMusic.Play ();
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") {
+			music = true;
+
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		music = false;
+	}
+}
